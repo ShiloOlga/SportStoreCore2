c0eb619ca4a646f5cc95dc3c9d660f923f3e64d4
Olga Shilo <olga.shilo@hiqo-solutions.com>
2018-12-05 11:04:59 +0300

    Add cart

---
+++ SportStoreCore2/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStoreCore2.Infrastructure;
+using SportStoreCore2.Models;
+using SportStoreCore2.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportStoreCore2.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public CartController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: /<controller>/
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            var product = _repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = _repository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                var cart = GetCart();
+                cart.Remove(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        private Cart GetCart()
+        {
+            var cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
---
+++ SportStoreCore2/Models/Cart.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportStoreCore2.Models
+{
+    public class Cart
+    {
+        private readonly List<CartLine> _lineCollection = new List<CartLine>();
+
+        public IEnumerable<CartLine> Lines => _lineCollection;
+
+        public void AddItem(Product product, int quantity)
+        {
+            var line = _lineCollection.FirstOrDefault(p => p.Product.ProductId == product.ProductId);
+            if (line == null)
+            {
+                _lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void Remove(Product product)
+        {
+            _lineCollection.RemoveAll(p => p.Product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return _lineCollection.Sum(p => p.Product.Price * p.Quantity);
+        }
+
+        public void Clear()
+        {
+            _lineCollection.Clear();
+        }
+    }
+
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
